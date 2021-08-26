01bf1cbe970addfcd6b148fe95d6db0cb4532535
bobagby78 <bobagby78@gmail.com>
2021-08-25 19:39:50 -0500

    RecipeIngredObj and MeasurementObj created to form List<ingredientObject> in recipe.java. recipe/add.html refactored to populate RecipeIngredObj.

--- WhatsSup/src/main/java/org/launchcode/WhatsSup/models/Recipe.java
+++ WhatsSup/src/main/java/org/launchcode/WhatsSup/models/Recipe.java
@@ -1,31 +1,49 @@
 package org.launchcode.WhatsSup.models;
 
 import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
 
 @Entity
 public class Recipe extends AbstractEntity{
     //id being extended from AbstrEnt
-
+    @NotBlank(message="Your recipe needs a title")
+    @Size (min=3, max = 250, message="Recipe title should be between 3 and 250 characters")
     private String recipeTitle;
+
+    @NotNull(message="Please tell us who added the recipe")
+    @Size (min=3, max=250, message="Recipe author should be at least 3 characters, but not exceed 250 characters")
     private String recipeAuthor;
+
     private int prepTimeMinutes;
+    @NotNull(message="Please enter a value for total cooking time")
     private int totalTimeMinutes;
+
     private int numServings;
+    @NotBlank(message="Please provide a brief description of your recipe")
+    @Size(min=3, max=500, message="Recipe description should not exceed 500 characters")
     private String recipeDescription;
-    private String ingredients; //
+
+    @NotBlank(message="It looks like  you haven't added any ingredients to your recipe")
+    private List<RecipeIngredObj> ingredients;
+
+    @NotBlank(message="It looks like  you haven't added any directions to your recipe")
     private String directions;
+
     private String notes;
 
     public Recipe(){}
 
-    public Recipe(String recipeTitle, String recipeAuthor, int prepTimeMinutes, int totalTimeMinutes, int numServings, String recipeDescription, String ingredients, String directions, String notes) {
+    public Recipe(String recipeTitle, String recipeAuthor, int prepTimeMinutes, int totalTimeMinutes, int numServings, String recipeDescription, List<RecipeIngredObj> ingredients, String directions, String notes) {
         this.recipeTitle = recipeTitle;
         this.recipeAuthor = recipeAuthor;
         this.prepTimeMinutes = prepTimeMinutes;
         this.totalTimeMinutes = totalTimeMinutes;
         this.numServings = numServings;
         this.recipeDescription = recipeDescription;
-        this.ingredients = ingredients;
+        this.ingredients = ingredients;//Cha
         this.directions = directions;
         this.notes = notes;
     }
@@ -78,11 +96,11 @@ public class Recipe extends AbstractEntity{
         this.recipeDescription = recipeDescription;
     }
 
-    public String getIngredients() {
+    public List<RecipeIngredObj> getIngredients() {
         return ingredients;
     }
 
-    public void setIngredients(String ingredients) {
+    public void setIngredients(List<RecipeIngredObj> ingredients) {
         this.ingredients = ingredients;
     }
 
---
+++ WhatsSup/src/main/java/org/launchcode/WhatsSup/models/RecipeIngredObj.java
@@ -0,0 +1,8 @@
+package org.launchcode.WhatsSup.models;
+
+import java.util.HashMap;
+
+public class RecipeIngredObj {
+    HashMap<Ingredient, MeasurementObj> ingredObj = new HashMap<>();
+
+}
