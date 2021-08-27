d8286ef0f4c6e00f7cc942f0037db9660bb8ff35
bobagby78 <bobagby78@gmail.com>
2021-08-26 20:26:36 -0500

    Started work on the MyKitchenController. Need to refactor the kitchen.html view. time to branch for that part of the project

--- WhatsSup/src/main/java/org/launchcode/WhatsSup/controllers/MyKitchenController.java
+++ WhatsSup/src/main/java/org/launchcode/WhatsSup/controllers/MyKitchenController.java
@@ -1,10 +1,20 @@
 package org.launchcode.WhatsSup.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("kitchen")
 public class MyKitchenController {
 
+    //Needs a repository for @Autowire
 
+    @GetMapping
+    public String displayMyKitchen(Model model){
+        model.addAttribute("title", "My Kitchen");
+        return"/kitchen";
+    }
 
 }
