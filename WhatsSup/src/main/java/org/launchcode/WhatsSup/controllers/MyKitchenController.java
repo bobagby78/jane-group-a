39cb0e7bd28fe15f9607511a415af3796ae50724
bobagby78 <bobagby78@gmail.com>
2021-08-28 15:18:49 -0500

    Got the add ingredient functionality moved over to MyKitchen. Posting info to the DB successfully

--- WhatsSup/src/main/java/org/launchcode/WhatsSup/controllers/MyKitchenController.java
+++ WhatsSup/src/main/java/org/launchcode/WhatsSup/controllers/MyKitchenController.java
@@ -1,20 +1,33 @@
 package org.launchcode.WhatsSup.controllers;
 
+import org.launchcode.WhatsSup.data.AddIngredientRepository;
+import org.launchcode.WhatsSup.models.Ingredient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("kitchen")
 public class MyKitchenController {
 
-    //Needs a repository for @Autowire
+    @Autowired
+    private AddIngredientRepository addIngredientRepository;
 
     @GetMapping
     public String displayMyKitchen(Model model){
-        model.addAttribute("title", "My Kitchen");
-        return"/kitchen";
+        model.addAttribute("title", "Hey, Dude. This is my kitchen");
+        model.addAttribute(new Ingredient());
+        return"kitchen/index";
+    }
+
+    @PostMapping
+    public String processAddIngredientForm(@ModelAttribute Ingredient newIngredient, Model model){
+        addIngredientRepository.save(newIngredient);
+        return "/kitchen/index";
     }
 
 }
