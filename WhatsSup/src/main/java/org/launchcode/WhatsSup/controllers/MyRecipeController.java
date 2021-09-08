a7b6ee7715056362bb28e9a9279a69b35f64f688
ricejake <ecirbocaj@yahoo.com>
2021-09-08 15:45:54 -0500

    MyRecipesController updated

--- WhatsSup/src/main/java/org/launchcode/WhatsSup/controllers/MyRecipeController.java
+++ WhatsSup/src/main/java/org/launchcode/WhatsSup/controllers/MyRecipeController.java
@@ -1,72 +1,35 @@
 package org.launchcode.WhatsSup.controllers;
-        import org.launchcode.WhatsSup.data.AddIngredientRepository;
-        import org.launchcode.WhatsSup.data.RecipeRepository;
-        import org.launchcode.WhatsSup.models.Ingredient;
-        import org.launchcode.WhatsSup.models.Recipe;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.stereotype.Controller;
-        import org.springframework.ui.Model;
-        import org.springframework.web.bind.annotation.GetMapping;
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.RequestParam;
 
-        import org.launchcode.WhatsSup.models.RecipeData;
+import org.launchcode.WhatsSup.data.RecipeRepository;
+import org.launchcode.WhatsSup.models.Recipe;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-        import java.util.HashMap;
-        import java.util.List;
-
+import java.util.List;
+@RestController
 
 @Controller
-@RequestMapping("myRecipe")
+@RequestMapping("/myrecipes")
 public class MyRecipeController {
-
     @Autowired
-    private RecipeRepository recipeRepository;
-    static HashMap<String, String> columnChoices = new HashMap<>();
+    private final RecipeRepository recipeRepository;
 
-    public MyRecipeController () {
+    public MyRecipeController(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+  }
 
-        columnChoices.put("all", "All");
-        columnChoices.put("recipes", "Recipes");
-        columnChoices.put("ingredients", "Ingredients");
+    @GetMapping("/myrecipes")
+    public List<Recipe> findByRecipeAuthor(@PathVariable("recipe_author") String recipeAuthor) {
+        return this.recipeRepository.findByRecipeAuthor(recipeAuthor);
+	}
 
-    }
-
-    @RequestMapping("")
-    public String list(Model model) {
-
-        return "recipes";
-    }
-
-
-
-//    @RequestMapping("")
-    public String recipes(Model model) {
-        Iterable<Recipe> recipes = recipeRepository.findAll();
-//        List<Ingredient> ingredients = AddIngredientRepository.findAll();
-
-        model.addAttribute("recipes", recipes);
-//        model.addAttribute("ingredients", ingredients);
-
-        return "recipes";
-    }
-
-
-
-
-
-//    @RequestMapping(value = "recipe")
-    public String listRecipesByColumnAndValue(Model model, @RequestParam String column, @RequestParam String value) {
-        Iterable<Recipe> recipes;
-        if (column.equalsIgnoreCase("all")){
-            recipes = recipeRepository.findAll();
-            model.addAttribute("title", "All Recipes");
-        } else {
-            recipes = RecipeData.findByColumnAndValue(column, value, recipeRepository.findAll());
-            model.addAttribute("title", "Recipes with " + columnChoices.get(column) + ": " + value);
-        }
-        model.addAttribute("recipes", recipes);
-
-        return "recipes";
+    @GetMapping("/byIngredients")
+    public List<Recipe> findByIngredients(@PathVariable("ingredients") String ingredients) {
+        return this.recipeRepository.findByIngredients(ingredients);
     }
 }
+
--- WhatsSup/src/main/java/org/launchcode/WhatsSup/data/RecipeRepository.java
+++ WhatsSup/src/main/java/org/launchcode/WhatsSup/data/RecipeRepository.java
@@ -1,7 +1,13 @@
 package org.launchcode.WhatsSup.data;
+
 import org.launchcode.WhatsSup.models.Recipe;
-import org.launchcode.WhatsSup.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public interface RecipeRepository extends CrudRepository<Recipe, Integer> {
+      List<Recipe> findByRecipeAuthor(String recipeAuthor);
+      List<Recipe> findByIngredients(String ingredients);
 }
