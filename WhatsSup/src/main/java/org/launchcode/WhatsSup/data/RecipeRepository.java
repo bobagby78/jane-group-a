5bd962ec3f0162ef80215e17dc118047eacccf02
ricejake <ecirbocaj@yahoo.com>
2021-09-13 16:23:13 -0500

    RecipeRepository update

--- WhatsSup/src/main/java/org/launchcode/WhatsSup/data/RecipeRepository.java
+++ WhatsSup/src/main/java/org/launchcode/WhatsSup/data/RecipeRepository.java
@@ -9,5 +9,6 @@ import java.util.List;
 @Repository
 public interface RecipeRepository extends CrudRepository<Recipe, Integer> {
       List<Recipe> findByRecipeAuthor(String recipeAuthor);
+//      List<Recipe> findByUserRecipe(String recipeAuthor = username);
       List<Recipe> findByIngredients(String ingredients);
 }
