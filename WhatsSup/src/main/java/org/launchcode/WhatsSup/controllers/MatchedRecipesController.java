2c74114a79027e3c587bd572cfc2beb32df82adf
bobagby78 <bobagby78@gmail.com>
2021-09-26 09:41:46 -0500

    add.html appends featuredIngredient index.html lists all recipes MatchedRecipesController prepped for matching based on featuredIngredient

--- WhatsSup/src/main/java/org/launchcode/WhatsSup/controllers/MatchedRecipesController.java
+++ WhatsSup/src/main/java/org/launchcode/WhatsSup/controllers/MatchedRecipesController.java
@@ -1,6 +1,7 @@
 package org.launchcode.WhatsSup.controllers;
 
 
+import org.launchcode.WhatsSup.data.AddIngredientRepository;
 import org.launchcode.WhatsSup.data.AddRecipeRepository;
 import org.launchcode.WhatsSup.data.RecipeRepository;
 import org.launchcode.WhatsSup.models.Ingredient;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
@@ -22,11 +25,38 @@ public class MatchedRecipesController {
     @Autowired
     private AddRecipeRepository addRecipeRepository;
 
+    @Autowired
+    private AddIngredientRepository addIngredientRepository;
+
 
     @GetMapping
     public String displayMatchedRecipes(Model model){
 
-        model.addAttribute("recipes", "Matched Recipes go here");
+        Iterable<Recipe> recipes;
+        recipes=addRecipeRepository.findAll();
+
+        ArrayList<Recipe> matchedRecipes = new ArrayList<>();
+        ArrayList<Recipe> userRecipes = new ArrayList<>();
+
+        Iterable<Ingredient> allIngredients = addIngredientRepository.findAll();
+        ArrayList<String> myIngredients = new ArrayList<>();
+
+
+        for (Ingredient ingredient : allIngredients){
+            myIngredients.add(ingredient.getIngredientName().toLowerCase());
+        }
+        for(Recipe recipe : recipes){
+
+            if(myIngredients.contains(recipe.getIngredients())) {
+                if (!matchedRecipes.contains(recipe)) {
+                    matchedRecipes.add(recipe);
+                }
+            }
+        }
+        model.addAttribute("recipes", recipes);
+
+
+//        model.addAttribute("recipes", addRecipeRepository.findAll());
                 //addRecipeRepository.findAll());
 //        }
 
@@ -35,8 +65,11 @@ public class MatchedRecipesController {
 
     @GetMapping("all")
     public String ListAllRecipes(Model model){
+        Iterable<Recipe> recipes;
 
-        model.addAttribute("recipes", addRecipeRepository.findAll());
+        recipes=addRecipeRepository.findAll();
+
+        model.addAttribute("recipes", recipes);
 
 
         return"matchedrecipes/all";
